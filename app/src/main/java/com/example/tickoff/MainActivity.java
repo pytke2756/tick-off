9a42d7955ad0cbe7fed562a6fd6e63ec7ffeb532
Levente Ibrányi <ibranyilevente@gmail.com>
2022-02-10 13:40:15 +0100

    Login tölt be alapból

--- app/src/main/java/com/example/tickoff/MainActivity.java
+++ app/src/main/java/com/example/tickoff/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tickoff;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+
+        int szam = 1;
+
+        if (szam == 1){
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
