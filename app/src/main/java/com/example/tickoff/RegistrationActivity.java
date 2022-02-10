7b9d79490c4716036e085ee0d121e9607600b06b
Levente Ibrányi <ibranyilevente@gmail.com>
2022-02-10 13:34:29 +0100

    Regisztráció alap activity

---
+++ app/src/main/java/com/example/tickoff/RegistrationActivity.java
@@ -0,0 +1,36 @@
+package com.example.tickoff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private Button regFrgmtBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        init();
+
+        regFrgmtBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new OtherRegFragment()).commit();
+                regFrgmtBtn.setText("Regisztráció");
+            }
+        });
+    }
+
+    private void init(){
+        regFrgmtBtn = findViewById(R.id.reg_frgmt_btn);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new BasicRegFragment()).commit();
+    }
+}
