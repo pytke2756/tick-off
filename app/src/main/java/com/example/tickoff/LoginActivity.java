824892c97101e369c825538c727cf50c9318efe1
Levente Ibrányi <ibranyilevente@gmail.com>
2022-02-24 21:08:35 +0100

    WiFi check

--- app/src/main/java/com/example/tickoff/LoginActivity.java
+++ app/src/main/java/com/example/tickoff/LoginActivity.java
@@ -2,8 +2,14 @@ package com.example.tickoff;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,6 +19,9 @@ public class LoginActivity extends AppCompatActivity {
 
     private Button regBtn;
     private TextView forgotPsw;
+
+    private WifiManager wifiManager;
+    private WifiInfo wifiInfo;
 
 
     @Override
@@ -41,8 +50,34 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        check();
+    }
+
+    private boolean isNetworkConnected(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+
+    private void check(){
+        if (!isNetworkConnected()){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+                Intent panel = new Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY);
+                startActivityForResult(panel, 0);
+            }
+            else{
+                wifiManager.setWifiEnabled(true);
+            }
+        }
+    }
+
     private void init(){
         regBtn = findViewById(R.id.reg_btn);
         forgotPsw = findViewById(R.id.forgot_psw);
+
+        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        wifiInfo = wifiManager.getConnectionInfo();
     }
 }
