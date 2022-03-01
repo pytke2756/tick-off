b657b00119caf9dc4ce24d5bb8a0f3b591f9b1c1
Levente Ibrányi <ibranyilevente@gmail.com>
2022-03-01 19:56:59 +0100

    Floating Action Button add

--- app/src/main/java/com/example/tickoff/Todos.java
+++ app/src/main/java/com/example/tickoff/Todos.java
@@ -7,11 +7,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Todos extends Fragment {
+
+    private FloatingActionButton todoAdd;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_todos, container, false);
+        View view = inflater.inflate(R.layout.fragment_todos, container, false);
+
+        init(view);
+
+        todoAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: a gombra nyomva felugrik a felvétel ablak
+                Toast.makeText(getContext(),"Hozzáad", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+
+    private void init(View view){
+        todoAdd = view.findViewById(R.id.add_todo);
     }
 }
