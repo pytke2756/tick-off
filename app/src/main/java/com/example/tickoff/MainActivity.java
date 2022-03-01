2306a044573f91b9ecf8ec29cae2d660171bae43
Levente Ibrányi <ibranyilevente@gmail.com>
2022-03-01 19:20:35 +0100

    Main screen, basic menu options, main, favorites, profile, logout

--- app/src/main/java/com/example/tickoff/MainActivity.java
+++ app/src/main/java/com/example/tickoff/MainActivity.java
@@ -1,22 +1,75 @@
 package com.example.tickoff;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity{
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int szam = 1;
+        getSupportActionBar().hide();
+
+        init();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Todos()).commit();
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(MainActivity.this,
+                drawerLayout, toolbar, R.string.open, R.string.close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.main:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Todos()).commit();
+                        break;
+                    case R.id.favorites:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Favorites()).commit();
+                        break;
+                    case R.id.profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Profile()).commit();
+                        break;
+                    case R.id.logout:
+                        Toast.makeText(MainActivity.this, "Kijelentkeztél", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+
+        int szam = 0;
 
         if (szam == 1){
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
         }
+    }
+
+    private void init(){
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigation);
     }
 }
