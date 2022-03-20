0a99e3da3194baa53160e2f0e9874dafdacc5819
Levente Ibrányi <ibranyilevente@gmail.com>
2022-03-20 12:49:26 +0100

    Register validation

--- app/src/main/java/com/example/tickoff/RegistrationActivity.java
+++ app/src/main/java/com/example/tickoff/RegistrationActivity.java
@@ -1,6 +1,7 @@
 package com.example.tickoff;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
@@ -8,35 +9,35 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONObject;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.Calendar;
-import java.util.HashMap;
 import java.util.regex.Pattern;
 
 import nu.aaro.gustav.passwordstrengthmeter.PasswordStrengthCalculator;
 import nu.aaro.gustav.passwordstrengthmeter.PasswordStrengthMeter;
 
-public class RegistrationActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, RequestTask.OutResponse{
+public class RegistrationActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
 
-    private Button regNextBtn;
-    private EditText regEmailEt;
-    private EditText regPwdEt;
-    private EditText regPwdAgainEt;
-    private EditText regFirstnameEt;
-    private EditText regLastnameEt;
-    private EditText regBirthEt;
-    private TextView regEmailError;
-    private TextView pwdMatchError;
-    private TextView regBirthError;
-    private RadioGroup regGenderGroup;
+    private MaterialButton regRegBtn;
+    private TextInputEditText regEmailEt;
+    private TextInputEditText regPwdEt;
+    private TextInputEditText regPwdAgainEt;
+    private TextInputEditText regFirstnameEt;
+    private TextInputEditText regLastnameEt;
+    private TextInputEditText regBirthEt;
+    private TextInputEditText regUsernameEt;
+    private AppCompatTextView regEmailErrorTv;
+    private AppCompatTextView pwdMatchErrorTv;
+    private AppCompatTextView regBirthErrorTv;
+    private AppCompatTextView regLastnameErrorTv;
+    private AppCompatTextView regFirstnameErrorTv;
+    private AppCompatTextView regUsernameErrorTv;
     private PasswordStrengthMeter meter;
-    
+
     private boolean pwdIsValid;
 
     @Override
@@ -57,10 +58,10 @@ public class RegistrationActivity extends AppCompatActivity implements DatePicke
                     String pwd = regPwdEt.getText().toString().trim();
                     String pwdAgain = regPwdAgainEt.getText().toString().trim();
                     if (!pwd.equals(pwdAgain)){
-                        pwdMatchError.setText("Nem egyezik a két jelszó!");
+                        pwdMatchErrorTv.setText("Nem egyezik a két jelszó!");
                     }
                     else{
-                        pwdMatchError.setText("");
+                        pwdMatchErrorTv.setText("");
                     }
                 }
             }
@@ -73,10 +74,10 @@ public class RegistrationActivity extends AppCompatActivity implements DatePicke
                     String pwd = regPwdEt.getText().toString().trim();
                     String pwdAgain = regPwdAgainEt.getText().toString().trim();
                     if (!pwd.equals(pwdAgain)){
-                        pwdMatchError.setText("Nem egyezik a két jelszó!");
+                        pwdMatchErrorTv.setText("Nem egyezik a két jelszó!");
                     }
                     else{
-                        pwdMatchError.setText("");
+                        pwdMatchErrorTv.setText("");
                     }
                 }
             }
@@ -128,8 +129,8 @@ public class RegistrationActivity extends AppCompatActivity implements DatePicke
                 birthPickerDialog();
             }
         });
-        
-        regNextBtn.setOnClickListener(new View.OnClickListener() {
+
+        regRegBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 boolean isValid = true;
@@ -137,6 +138,7 @@ public class RegistrationActivity extends AppCompatActivity implements DatePicke
                 cleanErrorMessages();
 
                 String email = regEmailEt.getText().toString().trim();
+                String username = regUsernameEt.getText().toString().trim();
                 String pwd = regPwdEt.getText().toString().trim();
                 String firstname = regFirstnameEt.getText().toString().trim();
                 String lastname = regLastnameEt.getText().toString().trim();
@@ -144,43 +146,42 @@ public class RegistrationActivity extends AppCompatActivity implements DatePicke
 
                 if (email.isEmpty()){
                     isValid = false;
-                    regEmailError.setText("Kötelező kitölteni!");
+                    regEmailErrorTv.setText(R.string.empty_input_error);
                 }
-                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
                     isValid = false;
-                    regEmailError.setText("Valós email címet kell megadni!");
+                    regEmailErrorTv.setText("Valós email címet kell megadni!");
                 }
 
-                if (pwd.equals("")){
+                if (username.isEmpty()){
                     isValid = false;
-                    pwdMatchError.setText("Kötelező kitölteni!");
+                    regUsernameErrorTv.setText(R.string.empty_input_error);
+                }
+
+                if (pwd.isEmpty()){
+                    isValid = false;
+                    pwdMatchErrorTv.setText(R.string.empty_input_error);
                 }
                 if (!pwdIsValid){
                     Toast.makeText(RegistrationActivity.this, "Nem felel meg a jelszavad", Toast.LENGTH_SHORT).show();
                 }
-                if (firstname.equals("")){
+                if (firstname.isEmpty()){
                     isValid = false;
+                    //TODO: kicsúszik a szöveg
+                    regFirstnameErrorTv.setText(R.string.empty_input_error);
                 }
-                if (lastname.equals("")){
+                if (lastname.isEmpty()){
                     isValid = false;
+                    //TODO: kicsúszik a szöveg
+                    regLastnameErrorTv.setText(R.string.empty_input_error);
                 }
-                int genderId = regGenderGroup.getCheckedRadioButtonId();
-                genderId -= 1;
-                if (genderId < 0){
+                if (birthDay.isEmpty()){
                     isValid = false;
+                    regBirthErrorTv.setText(R.string.empty_input_error);
                 }
                 if (isValid){
-                    HashMap<String, String> regData = new HashMap<>();
-                    regData.put("email", email);
-                    regData.put("username", "test");
-                    regData.put("password", pwd);
-                    regData.put("firstName", firstname);
-                    regData.put("lastName", lastname);
-
-                    JSONObject regJSON = new JSONObject(regData);
-                    RequestTask reg = new RequestTask(RegistrationActivity.this, "http://10.0.2.2:5000/api/register", "POST", regJSON.toString());
-                    reg.execute();
-                    //Asdfg123$123a
+                    //TODO: request api, regisztráció küldése, ha hiba van akkor mehetnek az errorokba
+                    Toast.makeText(RegistrationActivity.this, "Sikeres regisztráció", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -193,15 +194,18 @@ public class RegistrationActivity extends AppCompatActivity implements DatePicke
                 Calendar.getInstance().get(Calendar.YEAR),
                 Calendar.getInstance().get(Calendar.MONTH),
                 Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
-                );
+        );
         datePickerDialog.show();
 
     }
 
     private void cleanErrorMessages(){
-        regEmailError.setText("");
-        pwdMatchError.setText("");
-        regBirthError.setText("");
+        regEmailErrorTv.setText("");
+        regUsernameErrorTv.setText("");
+        pwdMatchErrorTv.setText("");
+        regBirthErrorTv.setText("");
+        regLastnameErrorTv.setText("");
+        regFirstnameErrorTv.setText("");
     }
 
     private boolean pwdDigitCheck(String password){
@@ -241,24 +245,26 @@ public class RegistrationActivity extends AppCompatActivity implements DatePicke
     }
 
     private void init(){
-        regNextBtn = findViewById(R.id.reg_next_btn);
+        regRegBtn = findViewById(R.id.reg_reg_btn);
         regEmailEt = findViewById(R.id.reg_email_et);
         regPwdEt = findViewById(R.id.reg_pwd_et);
+        regUsernameEt = findViewById(R.id.reg_username_et);
 
         regPwdAgainEt = findViewById(R.id.reg_pwd_again_et);
         regFirstnameEt = findViewById(R.id.reg_firstname_et);
         regLastnameEt = findViewById(R.id.reg_lastname_et);
         regBirthEt = findViewById(R.id.reg_birth_et);
 
-        regEmailError = findViewById(R.id.reg_email_error);
-        pwdMatchError = findViewById(R.id.pwd_match_error);
-        regBirthError = findViewById(R.id.reg_birth_error);
-
-        regGenderGroup = findViewById(R.id.reg_gender_group);
+        regEmailErrorTv = findViewById(R.id.reg_email_error_tv);
+        pwdMatchErrorTv = findViewById(R.id.pwd_match_error_tv);
+        regBirthErrorTv = findViewById(R.id.reg_birth_error_tv);
+        regLastnameErrorTv = findViewById(R.id.reg_lastname_error_tv);
+        regFirstnameErrorTv = findViewById(R.id.reg_firstname_error_tv);
+        regUsernameErrorTv = findViewById(R.id.reg_username_error_tv);
 
         meter = findViewById(R.id.pwd_meter);
         meter.setEditText(regPwdEt);
-        
+
         pwdIsValid = false;
     }
 
@@ -266,12 +272,5 @@ public class RegistrationActivity extends AppCompatActivity implements DatePicke
     public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
         String date = month + "/" + dayOfMonth + "/" + year;
         regBirthEt.setText(date);
-    }
-
-    @Override
-    public void response(Response response) {
-        if (response.getResponseCode() >= 400){
-            regEmailError.setText(response.getContent());
-        }
     }
 }
